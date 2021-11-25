cc60a24def7b039062a0a92365f5ce04597856a3
hai061898 <truonghaiauto98@gmail.com>
2021-11-25 10:04:50 +0700

    battery

--- lib/pages/home/home_sceen.dart
+++ lib/pages/home/home_sceen.dart
@@ -1,9 +1,13 @@
+// ignore_for_file: prefer_const_constructors, curly_braces_in_flow_control_structures
+
 import 'package:car/controllers/home_controller.dart';
 import 'package:car/pages/home/components/bottom_custom.dart';
 import 'package:car/pages/home/components/door_lock.dart';
 import 'package:car/utils/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+
+import 'components/bettery_status.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -12,18 +16,86 @@ class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
   final HomeController _controller = HomeController();
 
+  //----battery status -----/
+  late AnimationController _batteryAnimationController;
+  late Animation<double> _animationBattery;
+  late Animation<double> _animationBatteryStatus;
+
+  //----------temp----------//
+  late AnimationController _tempAnimationController;
   late Animation<double> _animationCarShift;
+  late Animation<double> _animationTempShowInfo;
+  late Animation<double> _animationCoolGlow;
+
+  void setupTempAnimation() {
+    _tempAnimationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 1500),
+    );
+    _animationCarShift = CurvedAnimation(
+      parent: _tempAnimationController,
+      curve: Interval(0.2, 0.4),
+    );
+    _animationTempShowInfo = CurvedAnimation(
+      parent: _tempAnimationController,
+      curve: Interval(0.45, 0.65),
+    );
+    _animationCoolGlow = CurvedAnimation(
+      parent: _tempAnimationController,
+      curve: Interval(0.7, 1),
+    );
+  }
+
+  void setupBatteryAnimation() {
+    _batteryAnimationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 600),
+    );
+    _animationBattery = CurvedAnimation(
+      parent: _batteryAnimationController,
+      curve: Interval(0.0, 0.5),
+    );
+    _animationBatteryStatus = CurvedAnimation(
+      parent: _batteryAnimationController,
+      curve: Interval(0.6, 1),
+    );
+  }
+
+  @override //khởi tạo 
+  void initState() {
+    setupBatteryAnimation();
+    setupTempAnimation();
+
+    super.initState();
+  }
+
+  @override // hủy biến tạm 
+  void dispose() {
+    _batteryAnimationController.dispose();
+    _tempAnimationController.dispose();
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return AnimatedBuilder(
-        animation: Listenable.merge([_controller]),
+        animation: Listenable.merge([
+          _controller,
+          _batteryAnimationController,
+        ]),
         builder: (context, _) {
           return Scaffold(
             bottomNavigationBar: BottomCustom(
-              onTap: (index) {},
+              onTap: (index) {
+                if (index == 1)
+                  _batteryAnimationController.forward();
+                else if (_controller.selectedBottomTab == 1 && index != 1)
+                  _batteryAnimationController.reverse(from: 0.7);
+              },
               selectedTab: _controller.selectedBottomTab,
             ),
             body: SafeArea(
@@ -35,8 +107,7 @@ class _HomeScreenState extends State<HomeScreen> {
                       width: constrains.maxWidth,
                     ),
                     Positioned(
-                      left:
-                          constrains.maxWidth, // 2 * _animationCarShift.value,
+                      left: constrains.maxWidth / 2 * _animationCarShift.value,
                       height: constrains.maxHeight,
                       width: constrains.maxWidth,
                       child: Padding(
@@ -49,7 +120,7 @@ class _HomeScreenState extends State<HomeScreen> {
                       ),
                     ),
 
-                    // door
+                    //----------- door--------------//
                     AnimatedPositioned(
                       duration: defaultDuration,
                       right: _controller.selectedBottomTab == 0
@@ -107,7 +178,26 @@ class _HomeScreenState extends State<HomeScreen> {
                       ),
                     ),
 
-                    
+                    //------battery -----/
+                    Opacity(
+                      opacity: _animationBattery.value,
+                      child: SvgPicture.asset(
+                        "assets/icons/Battery.svg",
+                        width: constrains.maxWidth * 0.45,
+                      ),
+                    ),
+
+                    Positioned(
+                      top: 50 * (1 - _animationBatteryStatus.value),
+                      height: constrains.maxHeight,
+                      width: constrains.maxWidth,
+                      child: Opacity(
+                        opacity: _animationBatteryStatus.value,
+                        child: BatteryStatus(constrains: constrains),
+                      ),
+                    ),
+
+                    //--------------------temp----------//
                   ]);
                 },
               ),
